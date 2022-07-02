fe7fd0ef82459db1add532ca491b9af44b2dba09
suryawanshi55 <107236171+suryawanshi55@users.noreply.github.com>
2022-07-02 20:27:02 +0530

    Add files via upload

---
+++ slicing_method.py
@@ -0,0 +1,28 @@
+# b='programming'
+# print(b[:11])   #programming
+# print(b[:6])      #progra
+# print(b[:3])     #pro
+# print(b[1:6])      #rogra
+# print(b[4:9])      #rammi
+# print(b[8:3])
+# print(b[3:11])     #gramming
+# print(b[5:5])
+# print(b[7:11])      #ming    
+# print(b[11:])
+# print(b[-11:])     #programming
+# print(b[-9:-2])    #ogrammi
+# v="DREAM PROJECT"
+# print(v[-1::])      #T
+# print(v[-1:-3:])
+# print(v[-5::-5])    #OA
+# print(v[::-3])   #TJPAD
+# print(v[-6:-9:-11])     #R
+# print(v[-9:-4:-5])
+# print(v[4:9])     #M PRO
+# print(v[2:6:])   #EAM
+# print(v[::-4])      #TOMD
+# print(v[::-11])     #TR
+# print(v[5:6:3])
+# print(v[::5])       #D E
+
+
